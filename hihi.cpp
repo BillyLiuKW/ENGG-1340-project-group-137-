07a57e8a92ad47dc2c1833d6e935211cb79695c3
BillyLiuKW <159022128+BillyLiuKW@users.noreply.github.com>
2024-04-11 19:53:02 +0800

    Update hihi.cpp

--- hihi.cpp
+++ hihi.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main() {
-   int a = 100;
-   string hong;
-   hong = "sing gor so small";
-   cout << a << endl;
-   cout << hong << endl;
 
-return 0;
-}
