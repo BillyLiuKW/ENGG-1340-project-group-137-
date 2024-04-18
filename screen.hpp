1c13a904077ec89c1e9fe393cbedffbce4ee6a03
pizza-without-pineapple <163004018+pizza-without-pineapple@users.noreply.github.com>
2024-04-18 20:32:45 +0800

    Update screen.hpp

--- screen.hpp
+++ screen.hpp
@@ -6,7 +6,6 @@
 #include <vector>
 
 using namespace std;
-typedef vector< vector<char> > array2d;
 struct format{
     int start_row = 0;
     int start_col = 0; // index of starting letter
@@ -16,31 +15,39 @@ struct format{
 
 class Screen{
     public:
-        //size of the screen
-        int height = 50, width = 50;
-        Screen();
+        int height, width;
+        vector<format> format_position = {};
+        vector<vector<char> > screen;
+        // default construction to make a screen
+        Screen(int height, int width) {
+            //cout << "e" << endl;
+            this->height = height; // assign the value of the size
+            this->width = width;
+            screen.resize(height, vector<char>(width, ' '));
+            create_edge();
+        };
 
-        //make the edge and conern of the screen
-        void create_edge(array2d &screen);
         // insert the item to the screen, remove all format information and put them in the format_position list
-        void insert_item(int start_row, int start_col, const vector<string> item, array2d &screen, vector<format> &format_position, vector<string> types);
+        void insert_item(int start_row, int start_col, const vector<string> item, vector<string> types);
         // print screen
-        void print_screen(array2d screen, vector<format> &format_position);
-        // only use in sort_list(), 
-        bool compareItems(const format &item1, const format &item2);
-        // sort the format_position by column position in descending order
-        void sort_list(vector<format> &format_position);
+        void print_screen();
         // insert the text format in the screen
-        void insert_format(array2d &screen, format f);
+        void insert_format(format f);
         // insert the text format in the format_position list
-        void formatting(int row, int col, string &line, vector<format> &format_position, int height_limit, int width_limit);
+        void formatting(int row, int col, string &line);
         // print all the format location
-        void print_format(vector<format> format_position);
+        void print_format();
         // insert the battelfield to the screen
         void insert_battelfield();
         // insert the information of the character
-        void insert_information();   
+        void insert_information();
+        //make the edge and conern of the screen
     private:
-};
+        void create_edge();
+        // only use in sort_list(), 
+        static bool compareItems(const format &item1, const format &item2);
+        // sort the format_position by column position in descending order
+        void sort_list();
 
+};
 #endif
