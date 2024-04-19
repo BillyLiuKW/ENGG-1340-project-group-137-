6bbe13156a3f955c7d311b0c04d1787009e9801f
pizza-without-pineapple <163004018+pizza-without-pineapple@users.noreply.github.com>
2024-04-19 10:19:46 +0800

    Update textformat.hpp

--- textformat.hpp
+++ textformat.hpp
@@ -9,8 +9,13 @@
 const std::map <std::string, std::string> formatMap = {
     {"reset", "\033[0m"},
     {"bold", "\033[1m"},
+    {"italics", "\033[3m"},
+    {"underline", "\033[4m"},
+    {"invert", "\033[7m"},
     {"red", "\033[31m"},
+    {"green", "\033[32m"},
     {"yellow", "\033[33m"},
+    {"purple", "\033[35m"},
     {"cyan", "\033[36m"}
 };
 
