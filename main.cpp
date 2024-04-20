1e9ee5b27c6b4a04c5ccc94f71d241b2c589b334
pizza-without-pineapple <163004018+pizza-without-pineapple@users.noreply.github.com>
2024-04-20 16:44:32 +0800

    fix syntax error
    can print the menu now
    improve indentation
    store some output into dialogs
    add break; to each switch case
    combine common code in switch case2 regardless if the files is found

--- main.cpp
+++ main.cpp
@@ -30,65 +30,76 @@ void menu() {
         "            #                                                 #            ",
         "           )(=================================================)(           ",  
     };
+    // print the menu
+    for (auto &line: test){
+        cout << line << endl;
+    }
 }
 // To handle the command input by player.
 void select_option(int option, GAME game) {
-      switch(option) {
+    switch(option) {
         case 1:
         { // game_status.txt exist only if the player has stored game status before.
-          // previous game status will be removed when player choose to start a new game.
-          ifstream fin("game_status.txt");
-          if (fin.good()) {
-          remove("game_status.txt");
-          }
-
-          MainCharacter m;
-          Enemy e(game.current_level);
-          cout << "Proceeding to level 1...." << endl; 
-          game.StartGame(m,e);
+            // previous game status will be removed when player choose to start a new game.
+            ifstream fin("game_status.txt");
+            if (fin.good()) {
+              remove("game_status.txt");
+            }
+            
+            MainCharacter m;
+            Enemy e(game.current_level);
+            cout << "Proceeding to level 1...." << endl;
+            sleep(1);
+            game.StartGame(m,e);
+            break;
         }
 
         case 2:
         {
-          ifstream fin("game_status.txt");
-          // check whether is game_status.txt existing.
-          if (fin.fail()) {
-            cout << "You haven't stored any game status!Start a new game instead." << endl;
-            main();
-          }
-          MainCharacter m;
-          // Assigning stored values to m one by one.
-          fin >> m.hp >> m.max_hp >> m.atk >> m.def;
-          // To input the size of moveSet stored.
-          int size;
-          fin >> size;
-          m.moveSet.resize(size);
-          for (int j; j < size; j++) {
-            fin >> m.moveSet[j];
-          }
-          // to read the level stored.
-          fin >> game.current_level;
-          //close the file after reading all data.
-          cout << "Game status successfully loaded" << endl;
-          fin.close();
-          Enemy e(game.current_level);
-          cout << "Proceeding to level " << game.current_level << endl;
-          game.StartGame(m,e);
-
+            MainCharacter m;
+            
+            ifstream fin("game_status.txt");
+            // check whether is game_status.txt existing.
+            if (fin.fail()) {
+                cout << "You haven't stored any game status!Start a new game instead." << endl;
+            }
+            else {
+                // Assigning stored values to m one by one.
+                fin >> m.hp >> m.max_hp >> m.atk >> m.def;
+                // To input the size of moveSet stored.
+                int size;
+                fin >> size;
+                m.moveSet.resize(size);
+                for (int j; j < size; j++) {
+                    fin >> m.moveSet[j];
+                }
+                // to read the level stored.
+                fin >> game.current_level;
+                //close the file after reading all data.
+                game.display.dialogs.insert(game.display.dialogs.begin(), "Game status successfully loaded"); // add the output message to dialog
+                fin.close();
+                cout << "Proceeding to level " << game.current_level << endl;
+            }
+            // Both case will do the below code
+            Enemy e(game.current_level);
+            sleep(1);
+            game.StartGame(m,e);
+            break;
         }
 
         case(5):
         {
-          char y_n;
-          cout << "Do you want to exit the game? [y/n]" << endl;
-          if (y_n == 'y') {
-            cout << "Thank you and goodbye!" << endl;
-            exit(0);
-          }
-
-          else {
-            main();
-          }
+            char y_n;
+            cout << "Do you want to exit the game? [y/n]" << endl;
+            if (y_n == 'y') {
+                cout << "Thank you and goodbye!" << endl;
+                exit(0);
+            }
+            
+            else {
+                main(); // this won't work
+            }
+            break;
         }
 
 
@@ -96,11 +107,11 @@ void select_option(int option, GAME game) {
 }
 
 int main() {
-  menu();
-  GAME game;
-  int option;
-  cout << "Select An Option: ";
-  cin >> option;
-  select_option(option,game);
-  return 0;
+    menu();
+    GAME game;
+    int option;
+    cout << "Select An Option: ";
+    cin >> option;
+    select_option(option,game);
+    return 0;
 }
