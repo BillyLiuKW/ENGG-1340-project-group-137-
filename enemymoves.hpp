e550d50f2c8a8dc79ac642174ab2b36860692493
pizza-without-pineapple <163004018+pizza-without-pineapple@users.noreply.github.com>
2024-04-21 20:27:01 +0800

    basically finish enemy move

--- enemymoves.hpp
+++ enemymoves.hpp
@@ -3,20 +3,47 @@
 
 #include <iostream>
 #include <vector>
-#include <iostream>
+#include <map>
+#include <utility>
 
 #include "character.hpp"
 using namespace std;
 
 class EnemyMoves{
     public:
+        // this map provide a template for the skill effect skill {{"e_hp 70"}}
+        map<string, function<void(EnemyMoves&, double, double)> > skill_option = {
+            {"e_hp", &EnemyMoves::e_hp},
+            {"m_hp", &EnemyMoves::m_hp},
+            {"e_atk", &EnemyMoves::e_atk},
+            {"m_atk", &EnemyMoves::m_atk},
+            {"e_def", &EnemyMoves::e_def},
+            {"m_def", &EnemyMoves::m_def},
+            {"e_hp", &EnemyMoves::e_hp},
+            {"m_hp", &EnemyMoves::m_hp},
+        };
+        MainCharacter &m;
+        Enemy &e;
+        vector<string> &dialogs;
         int type_num = 4; // number of type of skill
+        // set a in class variable so i don't need to pass to many function arguments
+        EnemyMoves(MainCharacter &main_c, Enemy &enemy, vector<string> &dialogs_list): m(main_c), e(enemy), dialogs(dialogs_list){}
         void Enemy_ExecuteMove(MainCharacter &m, Enemy &e, vector<string> &dialogs);
-        vector<double> z_score(vector<double> &skill_uses, vector<int> &skill_num, int& type_num_s);
+        // use normal attack
+        void normal_attack();
+        // use the chosen skill
+        void use_skill(int skill_id);
+        void boost_count_down(); 
     private:
+        vector<double> z_score(vector<double> &skill_uses, vector<int> &skill_num, int& type_num_s);
         int chooseSkillType(Enemy &e); // returm skill chosen in chooseSkill()
         int chooseSkill(vector<Enemy_Skill> skills); // only use in chooseSkillType()
-    
+        void e_hp(double multiplier, double other);
+        void m_hp(double multiplier, double other);
+        void e_atk(double multiplier, double other);
+        void m_atk(double multiplier, double other);
+        void e_def(double multiplier, double other);
+        void m_def(double multiplier, double other);
 };
 
 #endif
